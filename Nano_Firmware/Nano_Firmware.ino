fbe87ef98b5b1fd9553e3695af24856fc43c7dcc
Westpol <81779508+Westpol@users.noreply.github.com>
2023-06-04 18:18:12 +0200

    Added voltage measurement and cell voltage

--- Nano_Firmware/Nano_Firmware.ino
+++ Nano_Firmware/Nano_Firmware.ino
@@ -15,17 +15,36 @@
 #define cell5Pin A6
 #define cell6Pin A7
 
+#define ALL_CELLS 7
+
+int cellPins[6] = {cell1Pin, cell2Pin, cell3Pin, cell4Pin, cell5Pin, cell6Pin};
+float cellVoltages[6] = {0, 0, 0, 0, 0, 0};
+
+unsigned long voltageUpdaterTimer = 0;
+int voltageUpdaterTimerDelay = 100;
 
 void setup() {
   pinMode(selfActivationPin, OUTPUT);
   pinMode(tonePin, OUTPUT);
   pinMode(knobPin, INPUT_PULLUP);
+  
+  pinMode(cell1Pin, INPUT);
+  pinMode(cell2Pin, INPUT);
+  pinMode(cell3Pin, INPUT);
+  pinMode(cell4Pin, INPUT);
+  pinMode(cell5Pin, INPUT);
+  pinMode(cell6Pin, INPUT);
+
   success_tone();
   
 }
 
 void loop() {
   
+  if(voltageUpdaterTimer < millis()){
+    update_cell_voltage();
+    voltageUpdaterTimer = millis() + voltageUpdaterTimerDelay;
+  }
 
 }
 
@@ -37,5 +56,13 @@ void success_tone(){
 
   for(int i = 0; i < arr_length; i++){
     tone(tonePin, notes[i], durations[i]);
+  }
+}
+
+void get_cell_voltage(int cellNum){}
+
+void update_cell_voltage(){
+  for(int il = 0; il < 6; il++){
+    cellVoltages[il] = analogRead(cellPins[il]) * 5.0 / 1024.0 * (il + 1.0);
   }
 }
